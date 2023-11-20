922febddd85b7a93986fa7cb959f0d9d8618be0d
liminfinity <108136720+liminfinity@users.noreply.github.com>
2023-11-21 00:56:48 +0700

    Доработка ошибок

--- src/Logic/scenes/PreparationScene.js
+++ src/Logic/scenes/PreparationScene.js
@@ -1,14 +1,14 @@
 const shipDatas = [
-	{ size: 4, direction: "row", startX: 10, startY: 345 },
-	{ size: 3, direction: "row", startX: 10, startY: 390 },
-	{ size: 3, direction: "row", startX: 120, startY: 390 },
-	{ size: 2, direction: "row", startX: 10, startY: 435 },
-	{ size: 2, direction: "row", startX: 88, startY: 435 },
-	{ size: 2, direction: "row", startX: 167, startY: 435 },
-	{ size: 1, direction: "row", startX: 10, startY: 480 },
-	{ size: 1, direction: "row", startX: 55, startY: 480 },
-	{ size: 1, direction: "row", startX: 100, startY: 480 },
-	{ size: 1, direction: "row", startX: 145, startY: 480 },
+	{ size: 4, direction: "row", startX: 10, startY: 410 },
+	{ size: 3, direction: "row", startX: 10, startY: 455 },
+	{ size: 3, direction: "row", startX: 120, startY: 455 },
+	{ size: 2, direction: "row", startX: 10, startY: 500 },
+	{ size: 2, direction: "row", startX: 88, startY: 500 },
+	{ size: 2, direction: "row", startX: 167, startY: 500 },
+	{ size: 1, direction: "row", startX: 10, startY: 545 },
+	{ size: 1, direction: "row", startX: 55, startY: 545 },
+	{ size: 1, direction: "row", startX: 100, startY: 545 },
+	{ size: 1, direction: "row", startX: 145, startY: 545 },
 ];
 
 class PreparationScene extends Scene {
